36ba6b60937453002e802adc51acda6acfaa7499
rohan-namjoshi <148049895+rohan-namjoshi@users.noreply.github.com>
2024-10-23 10:32:57 +0530

    Update ques_6.py

--- SET-3/ques_6.py
+++ SET-3/ques_6.py
@@ -1,1 +1,17 @@
+def is_palindrome(str):
+    return str == str[::-1]
 
+
+def is_cyclic_palindrome(str):
+    n = len(str)
+    if is_palindrome(str):
+        return 1
+
+    for i in range (1,n):
+        str = str[1:] + str[0]
+        if is_palindrome(str):
+            return 1
+    return -1
+
+str = input("enter a string: ")
+print(is_cyclic_palindrome(str))
