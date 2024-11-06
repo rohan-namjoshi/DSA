9d262e674ea0aa09f80d0d459ee85a0d1e77d0ce
rohan-namjoshi <148049895+rohan-namjoshi@users.noreply.github.com>
2024-11-06 10:19:49 +0530

    Update ques_8.py

--- SET-3/ques_8.py
+++ SET-3/ques_8.py
@@ -1,1 +1,15 @@
+def combinations(c,n,s):
+  dp = [0]*(s+1)
+  dp[0] = 1
+  for i in c:
+    for j in range(i,s+1):
+      dp[j] += dp[j-i]
+  return dp[s]
+
+c = [1,2,3]
+n = len(c)
+s = 4
+
+print("combinations: ", combinations(c,n,s))
+
 
