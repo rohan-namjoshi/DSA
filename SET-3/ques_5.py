6ad865062229240f95135e26f1a3455129ecdaa3
rohan-namjoshi <148049895+rohan-namjoshi@users.noreply.github.com>
2024-12-04 09:52:00 +0530

    Create ques_5.py

---
+++ SET-3/ques_5.py
@@ -0,0 +1,23 @@
+def isValid(s):
+
+    stack = []
+
+    mapping = {")": "(", "}": "{", "]": "["}
+    
+    for char in s:
+      
+        if char in mapping:
+
+            top_element = stack.pop() if stack else '#'
+       
+            if mapping[char] != top_element:
+                return False
+        else:
+           
+            stack.append(char)
+
+    return not stack
+    
+x = ["()", "{"]
+
+print(is_valid(x))
